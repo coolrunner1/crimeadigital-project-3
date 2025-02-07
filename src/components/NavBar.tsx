d096b2b87f883b78e50d082d4b37fedfc3eb517e
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-02-07 18:56:59 +0300

    Layout fix for smaller screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import {useNavigate} from "react-router";
+import "./NavBar.css";
 
 export const NavBar = () => {
     const navigate = useNavigate();
 
     return (
         <>
-            <header className="flex flex-row justify-evenly shadow p-5 sm:pr-10 sm:pl-10 lg:pr-20 lg:pl-20 absolute z-40 w-screen">
+            <header className="flex flex-row justify-evenly shadow p-5 sm:pr-10 sm:pl-10 lg:pr-20 lg:pl-20 z-40 w-screen navbar">
                 <button onClick={() => navigate("/")}>Weather</button>
                 <button onClick={() => navigate("/search")}>Search</button>
                 <button onClick={() => navigate("/options")}>Options</button>
