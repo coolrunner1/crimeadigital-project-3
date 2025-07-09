40d952b1205bbb4a6fa22ee16c3b46e68e0edd2a
coolrunner1 <waurttttt@gmail.com>
2025-07-09 16:10:33 +0300

    Fixed an issue when w-screen leads to overflow in chromium-based browsers

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
     const navigate = useNavigate();
 
     return (
-        <header className="shadow p-5 pb-0 pt-3 sm:px-10 lg:px-20 z-40 w-screen navbar backdrop-blur-xl">
+        <header className="shadow p-5 pb-0 pt-3 sm:px-10 lg:px-20 z-40 w-screen max-w-full navbar backdrop-blur-xl">
             <div className="flex flex-row justify-evenly m-auto max-w-6xl">
                 <NavBarButton label={"Weather"} onClick={() => navigate("/")} />
                 <NavBarButton label={"Search"} onClick={() => navigate("/search")} />
